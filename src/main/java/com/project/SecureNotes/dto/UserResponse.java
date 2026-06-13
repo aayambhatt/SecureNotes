98a3e3f1b649bdddee4a0fed7f7ffae4998a7c63
aayambhatt <aayambhatt23@gmail.com>
2026-06-14 01:46:05 +0530

    UserResponse dto added

---
+++ src/main/java/com/project/SecureNotes/dto/UserResponse.java
@@ -0,0 +1,15 @@
+package com.project.SecureNotes.dto;
+
+
+import com.project.SecureNotes.entity.Role;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class UserResponse {
+
+    private UUID id;
+    private String email;
+    private Role role;
+}
