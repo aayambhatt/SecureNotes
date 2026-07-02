530802aabc81b9015e236c168449e5b4840a372c
aayambhatt <aayambhatt23@gmail.com>
2026-07-02 22:55:29 +0530

    WebConfig added, CORS issue solved.

---
+++ src/main/java/com/project/SecureNotes/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.project.SecureNotes.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Applies to ALL endpoints in the app
+                        .allowedOrigins("http://localhost:4200") // Allows your Angular app
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allows all headers (like Authorization headers later)
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
