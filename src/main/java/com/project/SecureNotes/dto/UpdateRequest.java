89bd6815244b06b5ec720a2d5119e788be47ac65
aayambhatt <aayambhatt23@gmail.com>
2026-08-05 01:28:20 +0530

    Validation in UpdateRequest dto added

--- src/main/java/com/project/SecureNotes/dto/UpdateRequest.java
+++ src/main/java/com/project/SecureNotes/dto/UpdateRequest.java
@@ -1,8 +1,10 @@
 package com.project.SecureNotes.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UpdateRequest {
+    @NotBlank
     private String name;
 }
